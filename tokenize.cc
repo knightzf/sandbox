2371765d3eacdad7fad4c9c62ec0cc33e0e3cb6d
knightzf <zhenfeit@gmail.com>
2021-07-13 21:40:45 -0700

    test
    Signed-off-by: knightzf <zhenfeit@gmail.com>

---
+++ tokenize.cc
@@ -0,0 +1,21 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::vector<std::string> tokenizer(const std::string& str) {
+    std::stringstream ss(str);
+    std::vector<std::string> res;    
+    for(std::string token; ss >> token; ) {
+        res.push_back(token);
+    }
+    return res;
+}
+
+int main() {
+    auto r = tokenizer("abc test -200.0");
+    for(const auto& s : r) 
+        std::cout<< s << std::endl;
+}
