37e1a4d182b8a47575e8edba3796a861144a4b2f
Zhenfei Tai <zhenfei.tai@addepar.com>
2025-03-15 10:59:53 -0700

    upd

--- proxy-extension/background.js
+++ proxy-extension/background.js
@@ -1,48 +1,3 @@
-// // Define the PAC script as a string
-// const pacScript = `
-// function FindProxyForURL(url, host) {
-//   if (shExpMatch(host, "*x.com") ||
-//       shExpMatch(host, "*twitter.com") ||
-//       shExpMatch(host, "*twimg.com") ||
-//       shExpMatch(host, "*whatismyip.com")) {
-//     return "PROXY 129.146.172.227:3128"";
-//   }
-//   return "DIRECT";
-// }
-// `;
-
-// // Set the proxy configuration
-// function setProxy() {
-//   const config = {
-//     mode: "pac_script",
-//     pacScript: {
-//       data: pacScript
-//     }
-//   };
-
-//   chrome.proxy.settings.set(
-//     { value: config, scope: "regular" },
-//     () => {
-//       if (chrome.runtime.lastError) {
-//         console.error("Error setting proxy:", chrome.runtime.lastError);
-//       } else {
-//         console.log("Proxy settings applied successfully.");
-//       }
-//     }
-//   );
-// }
-
-// // Apply proxy settings when the extension starts
-// setProxy();
-
-/*
-
-      "*.x.com",
-      "*.twitter.com"
-
-      
-      */
-
 // Define the proxy server details
 const proxyServer = {
   scheme: "http", // or "https", "socks5", etc.
@@ -56,7 +11,9 @@ const pacScript = `
     // List of websites to proxy (use hostnames or wildcards)
     const proxySites = [
       "*.twitter.com",
+      "twitter.com",
       "*.x.com",
+      "x.com",
       "*.twimg.com",
       "*.whatismyip.com"
     ];
